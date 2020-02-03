e9453d96db258391f05274cad2285a17e573b52b
Andrew Obiano <andrew.obiano@gmail.com>
2020-02-03 01:44:43 -1000

    final version of project

---
+++ Arm Simulator/Assets/Scripts/ArmController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+
+    public string keyPositive;
+    public string keyNegative;
+
+    public float targetVelocity;
+    public float force;
+    
+    Rigidbody rb;
+    HingeJoint joint;
+    JointMotor motor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        joint = GetComponent<HingeJoint>();
+
+        joint.useMotor = true;
+        
+        // motor.force = 100;
+        // motor.targetVelocity = torque;
+        // joint.motor = motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        motor = new JointMotor();
+        // joint.motor.force;
+
+        // Vector3 position = this.transform.position;
+        // position.x--;
+        // this.transform.position = position;
+
+
+        // KeyCode.LeftArrow
+        
+        if (Input.GetKey((KeyCode)System.Enum.Parse(typeof(KeyCode), keyPositive)))
+        {
+            motor.force = force;
+            motor.targetVelocity = targetVelocity;
+        }else if (Input.GetKey((KeyCode)System.Enum.Parse(typeof(KeyCode), keyNegative)))
+        {
+            motor.force = force;
+            motor.targetVelocity = -targetVelocity;
+        }
+        else
+        {
+            motor.force = 0;
+            motor.targetVelocity = 0;
+        }
+
+        joint.motor = motor;
+
+        // float turn = Input.GetAxis("Horizontal");
+        // rb.AddTorque(Vector3.forward * torque * turn);
+
+        // if (Input.GetKeyDown(KeyCode.LeftArrow))
+        // {
+        //     // Vector3 position = this.transform.position;
+        //     // position.x--;
+        //     // this.transform.position = position;
+        // }
+
+    }
+}
---
+++ Arm Simulator/Assets/Scripts/HammerSFX.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerSFX : MonoBehaviour
+{
+
+
+    public AudioClip nail;
+    public AudioClip nailTwo;
+    public AudioClip wood;
+    public AudioClip woodTwo;
+
+    int count;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        count = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "nail" && count % 2 == 0)
+        {
+            Debug.Log("nail");
+            audioSource.PlayOneShot(nail, 0.7F);
+            count++;
+        }
+        else if (collision.gameObject.tag == "nail" && count % 2 != 0)
+        {
+            Debug.Log("nailTwo");
+
+            audioSource.PlayOneShot(nailTwo, 0.7F);
+            count++;
+        }
+        if (collision.gameObject.tag == "wood" && count % 2 == 0)
+        {
+            Debug.Log("wood");
+
+            audioSource.PlayOneShot(wood, 0.7F);
+            count++;
+        }
+        else if (collision.gameObject.tag == "wood" && count % 2 != 0)
+        {
+            Debug.Log("woodTwo");
+
+            audioSource.PlayOneShot(woodTwo, 0.7F);
+            count++;
+        }
+    }
+
+}
